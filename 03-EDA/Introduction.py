6d8b7228fe81784172916b6ce5744d929a477897
Bryan Michalek <bryanmichalek@Bryans-MacBook-Pro-3.local>
2023-09-25 18:26:36 -0500

    updates to dashboard

---
+++ 03-EDA/Introduction.py
@@ -0,0 +1,17 @@
+# Libraries
+import streamlit as st
+
+# Page configuration
+st.set_page_config(
+    page_title="Dashboard Main",
+    layout="wide"
+)
+
+# Write introductory statement
+st.write("""
+         # Introduction
+         Hey how are ya.
+         """)
+
+# Prompt user to select the page they want
+st.sidebar.success("Select a page above.")
--- 03-EDA/pages/2_Individual_Skaters.py
+++ 03-EDA/pages/2_Individual_Skaters.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import sqlalchemy
 import plotly.graph_objects as go 
 
+############# CONFIGURATION #############
 # Set wide page layout 
-st.set_page_config(layout='wide')
+st.set_page_config(page_title='Individual Skaters',
+                   layout='wide')
 
+############# DATA LOAD #############
 @st.cache_data
 def load_data():
     # Create the engine to connect to the MySQL database
@@ -38,23 +40,34 @@ def load_data():
 # Load data into streamlit cache
 data = load_data()
 
+############# HEADERS/TITLE #############
 # Streamlit app
 st.title("Feature exploration")
 
+############# SIDEBAR #############
 # Create a sidebar for user input
 st.sidebar.title("User Input")
 
+# Drop down to select a metric
+metric_list = [col for col in data.columns if '_3' in col]
+default_metric = 'G60_3'
+selected_metric = st.sidebar.selectbox("Select a metric", metric_list, index=metric_list.index(default_metric))
+
 # Dropdown to select a player
 #selected_player = st.selectbox("Select a player", data['id_name'].unique())
-selected_players = st.sidebar.multiselect("Select players", data['id_name'].unique())
+selected_players = st.sidebar.multiselect("Select players", data['id_name'].unique(), default=['Connor McDavid - /m/mcdavco01'])
 
 # Drop down to select a season
-selected_season = st.sidebar.radio("Select a season", data['season'].unique())
+seasons_list = data['season'].unique().tolist()
+default_season = data['season'].max()
+selected_season = st.sidebar.radio("Select a season", seasons_list, index=seasons_list.index(default_season))
 
+############# DATA FILTERING #############
 # Filter the DataFrame based on the selected player
 #filtered_df = data[data['player_id'] == selected_player]
 filtered_df = data[(data['id_name'].isin(selected_players)) & (data['season'] == selected_season)]
 
+############# PLOTS #############
 # Create a line plot with points for the selected players
 fig = go.Figure()
 
@@ -63,7 +76,7 @@ for player in selected_players:
         player_data = filtered_df[filtered_df['id_name'] == player]
         fig.add_trace(go.Scatter(
                         x=player_data['date'],
-                        y=player_data['S60_3'],
+                        y=player_data[selected_metric],
                         mode='lines+markers',
                         name=player_data['name'].iloc[0]
                     )
@@ -73,12 +86,10 @@ for player in selected_players:
         continue
 
 fig.update_layout(
-    title='Shots per 60 (rolling 3)',
+    title='Rolling window: 3',
     xaxis_title='Date',
-    yaxis_title='Shots per 60',
+    yaxis_title=selected_metric,
     legend=dict(title='Player Names')
 )
 
-# Display the plot
 st.plotly_chart(fig)
-
