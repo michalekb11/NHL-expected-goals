cf3cf4da6242782b22c76fc2ed3d27d659c5b8f2
michalekb11 <bryan.michalek@yahoo.com>
2024-01-03 11:22:20 -0600

    finished template pytorch code

--- 04-modeling/deep_learning/model.py
+++ 04-modeling/deep_learning/model.py
@@ -1,14 +1,17 @@
 from torch import nn
 from torch.utils.data import Dataset
+import torch
+import pandas as pd
 
+# Define the NN model
 class NeuralNetwork(nn.Module):
     def __init__(self, n_features):
         super().__init__()
         self.dropout = nn.Dropout(p=0.2)
         self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(n_features, 100)
-        self.fc2 = nn.Linear(100, 50)
-        self.fc3 = nn.Linear(50, 1)
+        self.fc1 = nn.Linear(n_features, 50)
+        self.fc2 = nn.Linear(50, 20)
+        self.fc3 = nn.Linear(20, 1)
     
     def forward(self, x):
         x = self.fc1(x)
@@ -20,6 +23,7 @@ class NeuralNetwork(nn.Module):
         x = self.fc3(x)
         return x
 
+# Custom dataset class that takes in features as tensor and targets as any iterable?
 class NHL_Dataset(Dataset):
     def __init__(self, features, targets):
         self.features = features
@@ -33,3 +37,12 @@ class NHL_Dataset(Dataset):
         sample = self.features[idx]
         target = self.targets[idx]
         return sample, target
+    
+def prep_for_dataloader(df):
+    # Separate all index related information (player name, date, team, etc.)
+    index = pd.concat([df.pop(col) for col in ['player_name', 'team', 'opponent', 'date', 'goalie_name']], axis=1)
+    # Separate target variable (goals scored in that game)
+    targets = torch.tensor(df.pop('G'), dtype=torch.float32)
+    # Convert dataframe of features into tensor
+    features = torch.tensor(df.values, dtype=torch.float32)
+    return index, features, targets
--- 04-modeling/deep_learning/train.py
+++ 04-modeling/deep_learning/train.py
@@ -1,65 +1,114 @@
 import torch
 import pandas as pd
-from model import NHL_Dataset, NeuralNetwork
+from model import NHL_Dataset, NeuralNetwork, prep_for_dataloader
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
-# Set parameters
+############# Set model parameters #############
+########################################################
+output_model_name = './04-modeling/deep_learning/models/network1.pth'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-epochs = 2
-batch_size = 32
-learning_rate = 0.01
+epochs = 10
+batch_size = 8
+learning_rate = 0.005
+
+############# Gather features into dataset #############
+########################################################
+# Set up SQL queries to gather features
+train_query = """"""
+test_query = """"""
 
 # Import data into pandas dataframe from SQL
+#train_df = pd.read_sql(train_query)
+#test_df = pd.read_sql(test_query)
 
-# Separate all index related information (player name, date, team, etc.)
+# FOR TESTING PURPOSES  ONLY
+train_df = pd.read_csv('~/Desktop/train.csv')
+test_df = pd.read_csv('~/Desktop/test.csv')
 
-# Separate target variable (goals scored in that game)
-
-# Convert dataframe of features into tensor
+# Separate index info from features from targets (convert features and targets to tensors as well)
+train_index, train_features, train_targets = prep_for_dataloader(train_df)
+test_index, test_features, test_targets = prep_for_dataloader(test_df)
 
 # Set custom dataset class
-train_data = NHL_Dataset(features=features, targets=targets)
+train_data = NHL_Dataset(features=train_features, targets=train_targets)
+test_data = NHL_Dataset(features=test_features, targets=test_targets)
 
 # Set up the data loader
-dataloader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
+train_dataloader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=False)
 
+############# Train the model #############
+########################################################
 # Set up mode, optimizer, loss function
-model = NeuralNetwork(n_features=train_data.n_features)
-#optimizer = 
-# criterion = 
+model = NeuralNetwork(n_features=train_data.n_features).to(device=device)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+criterion = torch.nn.MSELoss()
 
 # Set up a train loop that occurs once per epoch
-def train_loop(dataloader, model, loss_fn, optimizer):
+def train_loop(dataloader, model, criterion, optimizer):
     # Set to training mode
     model.train()
 
     # Iterate through each batch
-    for batch, (X, y) in enumerate(dataloader):
+    for batch, (X, y) in enumerate(tqdm(dataloader, unit='batch')):
+        # Move data to device
+        X, y = X.to(device), y.to(device)
+
         # Compute predictions
+        preds = model(X)
+        preds = preds.flatten() # convert to size [batch_size] instead of [batch_size, 1] since this is the size of y
 
         # Compute loss
+        loss = criterion(preds, y)
 
         # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
 
-        # Every ___ batches, print the current loss as a message
+        # Every 50 batches, print the current loss as a message
+        if batch % 10 == 0:
+            print(f'Batch {batch} training loss: {loss.item()}')
 
 
 # Set up a test loop that occurs once per epoch
-def test_loop(dataloader, model, loss_fn):
+def test_loop(dataloader, model, criterion):
     # Set to eval mode
     model.eval()
+    loss = 0
 
     # Ensure no gradients are computed
-
+    with torch.no_grad():
         # For each batch, get predictions, compute loss and any other metrics
+        for X, y in dataloader:
+            # Move data to device
+            X, y = X.to(device), y.to(device)
+
+            # Get predictions and loss
+            preds = model(X)
+            preds = preds.flatten() # convert to size [batch_size] instead of [batch_size, 1] since this is the size of y
+            loss += criterion(preds, y).item() * len(X) # total SSE for the batch (since last batch might be less than batch size)
     
     # Print message about the test loss
+    loss = loss / len(dataloader.dataset) # MSE across entire test set
+    print(f'\nTest loss: {loss}\n')
+    
 
 # For each epoch
 for i in range(epochs):
-    print(f'Epoch {i}===================')
+    print(f'===== Epoch {i} ======================================================================================')
     # Run the train loop
-
+    train_loop(dataloader=train_dataloader, model=model, criterion=criterion, optimizer=optimizer)
     # Run the test loop
+    test_loop(dataloader=test_dataloader, model=model, criterion=criterion)
 
-print("Training complete.")
+print(f"Training complete\n")
+
+# Save the model
+try:
+    torch.save(model, output_model_name)
+    print('Model successfully saved')
+except:
+    print(f'Error saving model\n')
+    raise
