a08ab06f0fa204f54365802a925c9023a6778ed4
michalekb11 <bryan.michalek@yahoo.com>
2024-01-21 09:05:42 -0600

    daily scrape

--- 06-production/data_collection/webscrape/web_scrape.sh
+++ 06-production/data_collection/webscrape/web_scrape.sh
@@ -5,10 +5,10 @@ cd /Users/bryanmichalek/Documents/GitHub_Personal/sports_betting_repo/
 source .venv/bin/activate 
 
 # Run each web scrape script
-python 06-production/data_collection/web_scrape_schedule.py -v -N 2 2024
-python 06-production/data_collection/web_scrape_game_line_odds.py
-python 06-production/data_collection/web_scrape_60min_odds.py -t
-python 06-production/data_collection/web_scrape_anytime_scorer_odds.py -v -t
-python 06-production/data_collection/web_scrape_DF_line_combos.py -v -t
-python 06-production/data_collection/web_scrape_DF_goalies.py -v -t
-python 06-production/data_collection/web_scrape_rotowire_injury.py -v
+python 06-production/data_collection/webscrape/web_scrape_schedule.py -v -N 2 2024
+python 06-production/data_collection/webscrape/web_scrape_game_line_odds.py
+python 06-production/data_collection/webscrape/web_scrape_60min_odds.py -t
+python 06-production/data_collection/webscrape/web_scrape_anytime_scorer_odds.py -v -t
+python 06-production/data_collection/webscrape/web_scrape_DF_line_combos.py -v -t
+python 06-production/data_collection/webscrape/web_scrape_DF_goalies.py -v -t
+python 06-production/data_collection/webscrape/web_scrape_rotowire_injury.py -v
